007151961c877c97550e4d9ed1672ddad07b05b4
kiananonn <kiananonn@gmail.com>
2021-06-25 22:50:17 +0700

    main program

---
+++ Problem #2/problem 2.py
@@ -0,0 +1,6 @@
+print('''Welcome to Chula Chanal!!!
+Available commands:
+        1. Check in user
+        2. Check out user
+        3. Print people count''')
+command = input('Please input any number: ')
